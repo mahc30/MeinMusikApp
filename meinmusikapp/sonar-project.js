e5769ee64bcf61ae1b6c0ff80d54bd687364382a
mahc30 <dahinkpie@gmail.com>
2022-03-08 14:46:29 -0500

    sonarqube

---
+++ meinmusikapp/sonar-project.js
@@ -0,0 +1,19 @@
+const sonarqubeScanner = require('sonarqube-scanner')
+
+sonarqubeScanner({
+        serverUrl: "http://localhost:9000",
+        token: "",
+        options: {
+            "sonar.projectName": "MeinMusikApp",
+            "sonar.projectVersion": "0.0.1",
+            "sonar.projectDescription": "Spotify Api App",
+            "sonar.projectKey": "MeinMusikApp:Test",
+            "sonar.sources": ".",
+            "sonar.inclusions": "src/**",
+            "sonar.tests": "src",
+            "sonar.test.inclusions": "**/*.spec.ts",
+            "sonar.typescript.lcov.reportPaths": "coverage/lcov.info"
+        }
+    },
+    () => {}
+);
