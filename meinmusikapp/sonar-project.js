e59d04798ac9bcc2e7b7735de8dfafbf6d6ab54f
mahc30 <dahinkpie@gmail.com>
2022-03-09 10:38:58 -0500

    sonaqube url

--- meinmusikapp/sonar-project.js
+++ meinmusikapp/sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner')
 
 sonarqubeScanner({
-        serverUrl: "http://localhost:9000",
+        serverUrl: "http://sonarqube:9000",
         token: "",
         options: {
             "sonar.projectName": "MeinMusikApp",
